865af4d08bf8a5f6ce26cbd0af23246729fd1dce
fsaitzcap <capfsaitz@gmail.com>
2024-12-02 13:13:02 +0100

    Corrección de errores

--- src/main/java/com/ccsw/tutorial/client/ClientRepository.java
+++ src/main/java/com/ccsw/tutorial/client/ClientRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface ClientRepository extends CrudRepository<Client, Long> {
 
-    boolean existsByName(String name);
+    Boolean existsByName(String name);
 }
